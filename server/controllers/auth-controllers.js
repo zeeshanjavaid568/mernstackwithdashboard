ef3ec69e5b95e4a7626dbe36c8736718430f4766
ZEESHAN~ <zeeshan.568gb@gmail.com>
2023-12-06 14:53:23 +0500

    storing registration form data in database

--- server/controllers/auth-controllers.js
+++ server/controllers/auth-controllers.js
@@ -1,3 +1,6 @@
+ const User = require('../model/user-model');
+
+
 //========HOME-PAGE-FUNCTION===========
 
 const home = async (req, res) => {
@@ -13,7 +16,14 @@ const home = async (req, res) => {
 const register = async (req, res) => {
   try {
     console.log(req.body);
-    res.status(200).json({ message: req.body });
+    const {username, email, phone, password} = req.body;
+    const userExist = await User.findOne({email});
+    if (userExist) {
+      return res.status(400).json({message: 'email already exists.'});
+    }
+    const userCreated = await User.create({username, email, phone, password});
+    // const data = req.body;
+    res.status(200).json({message: userCreated});
   } catch (error) {
     res.status(400).send({ message: "Page not found." });
   }
